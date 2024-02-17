3a1e7e0963b2d6724aeadf73b29c48fb448bfd70
YC.W <contact@wangyuchen.cn>
2024-02-18 06:04:42 +0800

    chore: add as const

--- packages/dashboard/src/app/bh3/features/view/types.ts
+++ packages/dashboard/src/app/bh3/features/view/types.ts
@@ -2,10 +2,10 @@ export type OwnershipFilter = "both" | "true" | "false";
 
 export const DEFAULT_VIEW_STATE: ViewState = {
   ownershipFilter: "both",
-  showGroup: true
-};
+  showGroup: true,
+} as const;
 
 export type ViewState = {
   ownershipFilter: OwnershipFilter;
-  showGroup: boolean
+  showGroup: boolean;
 };
