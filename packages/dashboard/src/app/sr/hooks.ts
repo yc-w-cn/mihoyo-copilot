abdde8e1933eccda5c806850ede601edd5e99ada
YC.W <contact@wangyuchen.cn>
2024-02-18 07:58:15 +0800

    chore: remove useAppStore

--- packages/dashboard/src/app/sr/hooks.ts
+++ packages/dashboard/src/app/sr/hooks.ts
@@ -1,7 +1,6 @@
-import { useDispatch, useSelector, useStore } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
-import type { RootState, AppDispatch, AppStore } from './store'
+import type { RootState, AppDispatch } from './store'
 
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-export const useAppStore: () => AppStore = useStore
