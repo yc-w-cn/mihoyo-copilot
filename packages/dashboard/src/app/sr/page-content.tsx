ad9c15b3a3a9dcb55465ae0dcef6070a7a1eb51f
YC.W <contact@wangyuchen.cn>
2024-02-27 19:50:17 +0800

    fix: page header

--- packages/dashboard/src/app/sr/page-content.tsx
+++ packages/dashboard/src/app/sr/page-content.tsx
@@ -19,7 +19,7 @@ export function PageContent({ characters = [] }: PageContentProps) {
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-            原神
+            崩坏：星穹铁道
           </h1>
         </div>
         <Tabs defaultValue="image-text">
