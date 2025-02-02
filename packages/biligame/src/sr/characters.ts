f5ac481060accdee22061748c45aec096061f3a9
Yuchen Wang <contact@wangyuchen.cn>
2025-02-02 17:52:40 +0800

    fix: make it work

--- packages/biligame/src/sr/characters.ts
+++ packages/biligame/src/sr/characters.ts
@@ -25,20 +25,21 @@ export async function parseFn(html: string, options: ExecuteOptions) {
   const { biligameOptions } = options;
   const $ = cheerio.load(html);
   const characters = $(".chara-box")
-    .map((_, element) => ({
-      name: $(element).find(".chara-name a").text(),
-      image: $(element).find(".chara-image img").attr("src"),
-      element: $(element)
-        .find(".chara-element img")
-        .attr("alt")
-        .replace(".png", ""),
-      type: $(element).find(".chara-type img").attr("alt").split("-")[0],
-      rarity: $(element)
-        .find(".chara-rarity img")
-        .attr("alt")
-        .split("-")[1]
-        .replace(".png", ""),
-    }))
+    .map((_, element) => {
+      return {
+        name: $(element).find(".chara-name").text(),
+        image: $(element).find(".chara-image").attr("src"),
+        element: $(element)
+          .find(".chara-element")
+          .attr("alt")
+          .replace(".png", ""),
+        type: $(element).find(".chara-type").attr("alt").split("-")[0],
+        rarity: $(element)
+          .find(".chara-rarity")
+          .attr("alt")
+          .replace(".png", ""),
+      }
+    })
     .get();
 
   // download details
