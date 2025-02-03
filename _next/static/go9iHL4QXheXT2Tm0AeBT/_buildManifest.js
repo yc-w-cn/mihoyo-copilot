e95a8361179e614bec82786aac2933da35c0cbbd
Yuchen Wang <contact@wangyuchen.cn>
2025-02-03 21:19:44 +0800

    Updates

---
+++ _next/static/go9iHL4QXheXT2Tm0AeBT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7a5ffab65a3db747.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
